4097942f70b92917aa43e5ab09bfae73e21c80c5
Mohamed Lafir umair <99977258+DevoUmair@users.noreply.github.com>
2025-09-21 01:26:25 +0500

    update

--- src/components/FixedIcons/FixedIcons.js
+++ src/components/FixedIcons/FixedIcons.js
@@ -5,7 +5,7 @@ import {BsWhatsapp} from 'react-icons/bs'
 function FixedIcons() {
   return (
     <section class="fixed-icons">
-		<a href={`https://wa.me/60139277180?text= Hello 👋 Causeway Car Rental Johor Bahru?`} class="whatsapp" target="_blank" rel="noreferrer"><BsWhatsapp /> </a>
+		<a href={`https://wa.me/60166201542?text= Hello 👋 Causeway Car Rental Johor Bahru?`} class="whatsapp" target="_blank" rel="noreferrer"><BsWhatsapp /> </a>
 	</section>
   )
 }
